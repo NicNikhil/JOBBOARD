9c7494cf21dbfdc5572356be29ec7c2b5b0a7232
KHUSHI <khushi.s210929@gmail.com>
2023-11-22 14:24:59 +0530

    updated

--- server.js
+++ server.js
@@ -1,25 +1,42 @@
-// imports
+// packages imports
 //const express = require('express');
 
-import  express  from "express";
-import dotenv from "dotenv" ;
-import colors from "colors"
+import express from "express";
+import dotenv from "dotenv";
+import colors from "colors";
+import cors from 'cors';
+import morgan from "morgan";
+
+// files import
+import connectDB from "./config/db.js";
+// routes import
+import testRoutes from './routes/testRoutes.js';
+import authRoutes from './routes/authRoutes.js';
+
 
 //config dotenv
-dotenv.config()
+dotenv.config();
+
+//mongodb connection
+connectDB();
+
 
 //rest objects
 const app = express();
 
+//middelwares
+app.use(express.json());
+app.use(cors());
+app.use(morgan("dev"));
+
 // routes
-app.get('/' , (req , res) => {
-    res.send("<h1>Welcome to JOB PORTAL</h1>");
-})
+app.use('/api/v1/test', testRoutes);
+app.use('/api/v1/auth', authRoutes);
 //port
 const PORT = process.env.PORT || 8080
 //listen
 
 app.listen(PORT, () => {
-    console.log(`Node Server Running In ${process.env.DEV_MODE} Mode  on port no ${PORT}` 
-    .bgCyan.white);
+    console.log(`Node Server Running In ${process.env.DEV_MODE} Mode  on port no ${PORT}`
+        .bgCyan.white);
 })
